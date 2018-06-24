28e0236565e6b0ae60ecd6d7cd2b599ddd8b0beb
LLLyuujinn <531664389@163.com>
2018-06-24 13:48:45 +0900

    question1

---
+++ src/main/java/org/litespring/beans/factory/support/BeanDefinitionRegistry.java
@@ -0,0 +1,8 @@
+package org.litespring.beans.factory.support;
+
+/**
+ * @Author: lzy
+ * @Date: Created in 11:18 2018/6/24
+ */
+public class BeanDefinitionRegistry {
+}
---
+++ src/main/java/org/litespring/beans/factory/xml/XmlBeanDefinitionReader.java
@@ -0,0 +1,8 @@
+package org.litespring.beans.factory.xml;
+
+/**
+ * @Author: lzy
+ * @Date: Created in 11:10 2018/6/24
+ */
+public class XmlBeanDefinitionReader {
+}
